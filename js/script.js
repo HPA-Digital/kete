1a9d44618eb598cc3c5f15febe51728391ade865
Aran Mason <a.mason@hpa.org.nz>
2019-02-22 11:13:21 +1300

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+function test() {
+	//console.log('test');
+	//$('#container').addClass('magictime vanishOut');
+
+	// setTimeout(() => {
+	// 	$('#rant').val('');
+	// }, 500);
+
+	// $('#fire').css({
+	// 	top: `${-$('img.fire').height()}px`
+	// })
+
+	$('#fire').addClass('top');
+
+}
+
+$(document).ready(() => {
+
+	let params = (new URL(document.location)).searchParams;
+
+	let default_text = params.get('name') ? `Hey ${params.get('name')}, how is it going?` : 'How is it going?';
+	console.log('Text', default_text);
+	$('#header').text(default_text);
+})
+
