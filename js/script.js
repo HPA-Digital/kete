7375a60d7bcf23a8056815d63b9970bd272bcf62
Aran Mason <a.mason@hpa.org.nz>
2019-02-22 12:15:40 +1300

    Fixing SDK implementation?

--- js/script.js
+++ js/script.js
@@ -27,12 +27,13 @@ $(document).ready(() => {
 	let default_text = params.get('name') ? `Hey ${params.get('name')}, how is it going?` : 'How is it going?';
 	console.log('Text', default_text);
 	$('#header').text(default_text);
+
+	let hi = (function() {
+		alert('foo');
+	  })();
+
+	hi();
 })
 
-(function(d, s, id){
-	var js, fjs = d.getElementsByTagName(s)[0];
-	if (d.getElementById(id)) {return;}
-	js = d.createElement(s); js.id = id;
-	js.src = "https://connect.facebook.net/en_US/messenger.Extensions.js";
-	fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'Messenger'));
+
+
