cf02a43b4918f7eb90f15f28fd15a911d7adcb6e
Aran Mason <a.mason@hpa.org.nz>
2019-02-22 13:25:36 +1300

    Redcued the size of the text area

--- js/script.js
+++ js/script.js
@@ -1,18 +1,6 @@
 function test() {
-	//console.log('test');
-	//$('#container').addClass('magictime vanishOut');
-
-	// setTimeout(() => {
-	// 	$('#rant').val('');
-	// }, 500);
-
-	// $('#fire').css({
-	// 	top: `${-$('img.fire').height()}px`
-	// })
 
 	$('#fire').addClass('top');
-
-
 
 	setTimeout(() => {
 		alert("clsoed");
@@ -21,28 +9,14 @@ function test() {
 		}, function error(err) {
 			console.error(err);
 		});
-	}, 2000)
-
-
+	}, 2000);
 }
 
 $(document).ready(() => {
 	let params = (new URL(document.location)).searchParams;
 	let default_text = params.get('name') ? `Hey ${params.get('name')}, how is it going?` : 'How is it going?';
-	console.log('Text', default_text);
 	$('#header').text(default_text);
 })
-
-// window.extAsyncInit = function () {
-// 	alert("Loaded MSG SDK");
-
-// 	MessengerExtensions.getUserID(function success(uids) {
-// 	var psid = uids.psid; alert(psid);
-// }, function error(err) {
-// 	alert("Messenger Extension Error: " + err);
-// });
-//};
-
 
 var defaultPrevent=function(e){e.preventDefault();}
 elem.addEventListener("touchstart", defaultPrevent);
