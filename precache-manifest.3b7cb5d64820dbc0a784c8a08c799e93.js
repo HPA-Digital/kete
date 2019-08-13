76b7d4aa41e0737f40af3645fbd5b6fe4e71a413
Aran Mason <a.mason@hpa.org.nz>
2019-08-14 10:34:27 +1200

    Updates

---
+++ precache-manifest.3b7cb5d64820dbc0a784c8a08c799e93.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "332d5de873c0f3f75677854b2c19463b",
+    "url": "/Kete/index.html"
+  },
+  {
+    "revision": "3f1ccfbf3e603d7397fc",
+    "url": "/Kete/static/css/main.061e7bd3.chunk.css"
+  },
+  {
+    "revision": "63c46f596a70e4b6a992",
+    "url": "/Kete/static/js/2.6e75c9e5.chunk.js"
+  },
+  {
+    "revision": "3f1ccfbf3e603d7397fc",
+    "url": "/Kete/static/js/main.bd616808.chunk.js"
+  },
+  {
+    "revision": "ecddd7f86cdbc70d8e1f",
+    "url": "/Kete/static/js/runtime~main.9a68e765.js"
+  },
+  {
+    "revision": "4ec9ba7f6c7626a4add657610b99c7f6",
+    "url": "/Kete/static/media/KUINI_1.4ec9ba7f.jpg"
+  },
+  {
+    "revision": "09bf3fa19fa4deb44a1d1012317fe35b",
+    "url": "/Kete/static/media/KUINI_2.09bf3fa1.jpg"
+  },
+  {
+    "revision": "20f05a7786cc507531a7a103bec6575c",
+    "url": "/Kete/static/media/KUINI_3.20f05a77.jpg"
+  },
+  {
+    "revision": "86ba644d43c26835cd3e748e745683d7",
+    "url": "/Kete/static/media/KUINI_4.86ba644d.jpg"
+  },
+  {
+    "revision": "3436bc8e19121609e054dd30aabd37e9",
+    "url": "/Kete/static/media/KUINI_5.3436bc8e.jpg"
+  },
+  {
+    "revision": "5cc6fadfbf42ff57d6a4993dcc37ef2b",
+    "url": "/Kete/static/media/KUINI_6.5cc6fadf.jpg"
+  },
+  {
+    "revision": "b1cc9121e33b113e6ea87150ad9d52cf",
+    "url": "/Kete/static/media/KUINI_K.b1cc9121.png"
+  },
+  {
+    "revision": "ff5b4c4e61252f8f9a8da2806bce3f26",
+    "url": "/Kete/static/media/fast-forward.ff5b4c4e.svg"
+  },
+  {
+    "revision": "a406bff1575541b1e603959dff544610",
+    "url": "/Kete/static/media/star.a406bff1.svg"
+  }
+]);
